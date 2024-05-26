134445ae95c1f2f423d279b613f2d8d06c959b44
mohsen-jafari <www.mohsenofficial1@gmail.com>
2024-05-26 20:55:29 +0430

    fetch api

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-// api.openweathermap.org/data/2.5/forecast?q={city name}&appid={API key}
+//
 
 const apiKey = "9384f52d685e11de01e40017d52bc800";
+
+const form = document.querySelector(".form-container form");
+const cityInput = document.querySelector("form #city-input");
+
+const cities = document.querySelector(".weekdays");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const inputValue = cityInput.value;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputValue}&appid=${apiKey}&units=metric`;
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      const { list, city } = data;
+      list.forEach((item) => {
+        console.log(item.dt_txt);
+      });
+    });
+});
